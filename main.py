c8a4aabbd8b5ed7fa651e73729776a891d390703
korenerok <marvin.bernal@gmx.com>
2023-01-12 18:43:54 -0400

    Updated code for current Telegram library version

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Application, CommandHandler
 from configparser import ConfigParser
 import logging
 
@@ -6,30 +6,32 @@ config = ConfigParser()
 configfile = 'bot.conf'
 
 config.read(f"./{configfile}")
-token = config['SETTING_BOT']['token']
+token = config['SETTINGS_BOT']['token']
 
-def start(update):
-    update.message.reply_text("Greetings!")
+async def start(update,context):
+    user = update.effective_user
+    await update.message.reply_text(f"Greetings {user.username} !")
 
-def help(update):
+async def help(update,context):
     msg = """/help = List all available commands.
-            /start = Gives a hearty salutation.
-            /chat_id = Returns the chat id where the bot responds to. 
+/start = Gives a hearty salutation.
+/chat_id = Returns the chat id where the bot responds to. 
             """
-    update.message.reply_text(msg)
+    await update.message.reply_text(msg)
 
-def chatId(update):
+async def chatId(update,context):
     chatId = update.message.chat.id
-    update.message.reply_text(chatId)
+    await update.message.reply_text(chatId)
 
-if __name__ == '__main__':
-    updater = Updater(token=token, use_context=True)
-    dispatcher = updater.dispatcher
+def main():
+    application = Application.builder().token(token).build()
     logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-    dispatcher.add_handler(CommandHandler('chat_id', chatId))
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CommandHandler('help', help))
-    dispatcher.add_handler(CommandHandler('helpme', help))
-    updater.start_polling()
+    application.add_handler(CommandHandler('chat_id', chatId))
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('help', help))
+    application.add_handler(CommandHandler('helpme', help))
+    application.run_polling()
     print('Bot started')
-    updater.idle()
+
+if __name__ == "__main__":
+    main()
